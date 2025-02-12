b66d8a9dd15cacdd55f69e3d2535a0bd67529165
Lola Castan <lcastan.pro@gmail.com>
2025-02-12 15:53:15 +0100

    Module bots initié

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { TablesService } from './tables/tables.service';
 import { ActionsController } from './actions/actions.controller';
 import { ActionsService } from './actions/actions.service';
 import { CardsModule } from './cards/cards.module';
+import { BotsModule } from './bots/bots.module';
 
 @Module({
-  imports: [CardsModule],
+  imports: [CardsModule, BotsModule],
   controllers: [AppController, TablesController, ActionsController],
   providers: [AppService, TablesService, ActionsService],
 })
